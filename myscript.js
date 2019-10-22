c3d6fa3840fd9c680925f697a04c8de13bf3a433
Boureima Tapily <boureimatapily1@gmail.com>
2019-10-22 16:46:14 +0000

    linking js file

---
+++ myscript.js
@@ -0,0 +1,12 @@
+// operation
+var numb1 = 20
+var numb2 = 10
+
+console.log("numb1" + "=" + numb1)
+console.log("numb2" + "=" + numb2)
+console.log(numb1 + "+" + numb2 + "=", numb1 + numb2)
+console.log(numb1 + "-" + numb2 + "=", numb1 - numb2)
+console.log(numb1 + "*" + numb2 + "=", numb1 * numb2)
+console.log(numb1 + "/" + numb2 + "=", numb1 / numb2)
+console.log(numb1 + "%" + numb2 + "=", numb1 % numb2)
+
